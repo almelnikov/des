9fb29c832978f819a18e060d2ee809d7d9e6f1a9
dedigurova <alenadedigurova@yandex.ru>
2015-05-24 00:45:50 +0600

    first

---
+++ generators.hpp
@@ -0,0 +1,12 @@
+#ifndef GENERATORS_HPP
+#define GENERATORS_HPP
+
+#include <cstdlib>
+#include <ctime>
+#include <math.h>
+
+int Uniform(int bound);
+int Uniform(int left_bound, int right_bound);
+double Expon(double lambda);
+
+#endif
